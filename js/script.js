9a75233db392e60fce98899c0773f784975b8cf7
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-05 15:35:54 +0200

    [JS] Added Logic

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ const listElement = document.getElementById("lista-spesa");
 
 const shoppingList = ["Latte", "Uova", "Pane", "Nutella", "Kinder Bueno"];
 
+/*
 // Preparation ul
 
 let listItem = `<ul>`;
@@ -22,6 +23,29 @@ let listItem = `<ul>`;
 
 for (i = 0; i < shoppingList.length; i++) {
   listItem += `<li>${shoppingList[i]}</li>`;
+}
+
+// Closing ul
+
+listItem += `</ul>`;
+
+// I print the elements on page
+
+listElement.innerHTML = listItem;
+
+*/
+
+// Preparation ul
+
+let listItem = `<ul>`;
+
+// I create a cycle
+
+let i = 0;
+
+while (i < shoppingList.length) {
+  listItem += `<li>${shoppingList[i]}</li>`;
+  i++;
 }
 
 // Closing ul
