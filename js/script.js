3344238fd625dc1473f24656e12953bb487aa266
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-05 15:32:06 +0200

    [JS] Added Logic FOR

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log("JSOK");
+
+/*Recupero l'elemento dal DOM
+Creo una lista della spesa
+Creo un ciclo per leggere tutti gli elementi
+Stampo tutti gli elementi in pagina
+*/
+
+// Fetch item from Dom
+
+const listElement = document.getElementById("lista-spesa");
+
+// I create a shopping list
+
+const shoppingList = ["Latte", "Uova", "Pane", "Nutella", "Kinder Bueno"];
+
+// Preparation ul
+
+let listItem = `<ul>`;
+
+// I create a cycle
+
+for (i = 0; i < shoppingList.length; i++) {
+  listItem += `<li>${shoppingList[i]}</li>`;
+}
+
+// Closing ul
+
+listItem += `</ul>`;
+
+// I print the elements on page
+
+listElement.innerHTML = listItem;
